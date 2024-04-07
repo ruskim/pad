8f52d61466d85ca9025e0db3081de36cd4e7979a
ruskim <ruskim@gmail.com>
2024-04-07 17:35:02 +0300

    rotations

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,47 @@ fn build_piece(c: Vec<Vec<i8>>) -> Piece {
 }
 
 impl Piece {
-    fn print_piece(&mut self) {
+    fn print(&self) {
         for r in &self.cells {
             for c in r {
                 print!("{}", c);
             }
             println!("");
         }
-        self.cells[0].push(1);
+    }
+    
+    fn rotate(&self) -> Piece {
+        let mut i = 0;
+        let mut cells: Vec<Vec<i8>> =  Vec::new();
+        for r in &self.cells {
+            let mut j = 0;
+            for c in r {
+                if i == 0 {
+                    cells.push(Vec::new())
+                }
+                cells[j].push(*c);
+                j += 1;
+            }
+            i += 1;
+        };
+
+        i = 0;
+        while i<cells.len() {
+            cells[i].reverse();
+            i += 1;
+        }
+
+        build_piece(cells)
     }
 }
 
 fn main() {
-    let mut p = build_piece(vec![vec![0,1], vec![0,1]]);
-    p.print_piece();
-    p.print_piece();
+    let p = build_piece(vec![vec![0,1], vec![0,1]]);
+    let p2 = p.rotate();
+    let p3 = p2.rotate();
+    let p4 = p3.rotate();
+    p.print();
+    p2.print();
+    p3.print();
+    p4.print();
 }
