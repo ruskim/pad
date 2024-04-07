f4f4fa6cc524aa8e395e492d7f508d536996ab6b
ruskim <ruskim@gmail.com>
2024-04-07 16:00:53 +0300

    Piece struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+// use std::vec;
+
+struct Piece {
+    cells: Vec<Vec<i8>>,
+}
+
+fn build_piece(c: Vec<Vec<i8>>) -> Piece {
+    Piece {
+        cells: c,
+    }
+}
+
+fn print_piece(p: Piece) {
+    for r in p.cells {
+        for c in r {
+            print!("{}", c);
+        }
+        println!("");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p = build_piece(vec![vec![0,1], vec![0,1]]);
+    print_piece(p);
 }
