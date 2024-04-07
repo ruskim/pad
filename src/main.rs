de6f58b95de4a993072629ecc92e3a536f85c291
ruskim <ruskim@gmail.com>
2024-04-07 18:50:21 +0300

    pieces class

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,71 @@ struct Piece {
     cells: Vec<Vec<i8>>,
 }
 
+struct PieceClass {
+    pieces: Vec<Piece>,
+}
+
 fn build_piece(c: Vec<Vec<i8>>) -> Piece {
     Piece {
         cells: c,
+    }
+}
+
+fn has_piece( ps: &Vec<Piece>, p: &Piece) -> bool {
+    for i in ps {
+        if i.equal(p) {
+            return true;
+        }
+    }
+    false
+}
+
+fn build_piece_class(p: Piece) ->PieceClass {
+        let mut pieces: Vec<Piece> =  Vec::new();
+
+        let p1 = p.rotate();
+        let p2 = p1.rotate();
+        let p3 = p2.rotate();
+
+        let pf = p.flip();
+        let pf1 = pf.rotate();
+        let pf2 = pf1.rotate();
+        let pf3 = pf2.rotate();
+
+        pieces.push(p);
+        if !has_piece(&pieces, &p1) {
+            pieces.push(p1)
+        }
+        if !has_piece(&pieces, &p2) {
+            pieces.push(p2)
+        }
+        if !has_piece(&pieces, &p3) {
+            pieces.push(p3)
+        }
+        if !has_piece(&pieces, &pf) {
+            pieces.push(pf)
+        }
+        if !has_piece(&pieces, &pf1) {
+            pieces.push(pf1)
+        }
+        if !has_piece(&pieces, &pf2) {
+            pieces.push(pf2)
+        }
+        if !has_piece(&pieces, &pf3) {
+            pieces.push(pf3)
+        }
+
+        PieceClass {
+            pieces: pieces,
+        }
+
+}
+
+impl PieceClass {
+    fn print(&self) {
+        for r in &self.pieces {
+            r.print();
+        }
     }
 }
 
@@ -87,18 +149,6 @@ impl Piece {
 
 fn main() {
     let p = build_piece(vec![vec![1,1,0], vec![0,1,0],vec![0,1,1]]);
-    let p2 = p.rotate();
-    let p3 = p2.rotate();
-    let p4 = p3.rotate();
-    let pf = p.flip();
-
-    p.print();
-    p2.print();
-    println!("{}", p.equal(&p2));
-    p3.print();
-    println!("{}", p.equal(&p3));
-    p4.print();
-    println!("{}", p.equal(&p4));
-    pf.print();
-    println!("{}", p.equal(&pf));
+    let c = build_piece_class(p);
+    c.print();
 }
